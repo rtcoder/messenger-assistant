d31578654b8ed38edc4837adfd9c8098ffda3c43
Dawid Jeż <dawidjez@gmail.com>
2023-09-07 18:24:32 +0200

    valid return

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
-echo 'ok';
+if (!isset($_GET['hub_mode'])
+    || !isset($_GET['hub_verify_token'])
+    || !isset($_GET['hub_challenge'])
+) {
+    echo 'err';
+    return;
+}
+echo $_GET['hub_challenge'];
+http_response_code(200);
