4f9af7a5971e9177e80ea640a9137bdd84c91c01
Dawid Jeż <dawidjez@gmail.com>
2023-09-09 21:07:18 +0200

    debug

--- services/yt-dlp.js
+++ services/yt-dlp.js
@@ -5,61 +5,81 @@ const {getRandomString} = require('../utils/string-helpers');
 const getConfig = require('../utils/config');
 const config = getConfig('config');
 const linksDb = getDb('links');
+const {exec} = require('child_process');
 
 module.exports = {
     yt_dlp: async (cmd, messageContent, api, msg) => {
         try {
-
-            youtubedl(messageContent, {
-                dumpSingleJson: true,
-                noCheckCertificates: true,
-                noWarnings: true,
-                preferFreeFormats: true,
-                addHeader: [
-                    'referer:youtube.com',
-                    'user-agent:googlebot',
-                ],
-
-            }).then(output => {
-
-                const formats = output.formats.filter(f =>
-                    f.audio_ext === 'none' && f.format_note && f.audio_channels,
-                ).map(f => ({
-                    width: f.width,
-                    height: f.height,
-                    url: f.url,
-                    format_note: f.format_note,
-                    quality: f.quality,
-                    fps: f.fps,
-                }));
-
-                const bestVideo = formats.reduce((best, current) => {
-                    return current.quality > best.quality ? current : best;
-                }, formats[0]);
-
-                const found = Object.keys(linksDb).find(key => {
-                    return linksDb[key].url === bestVideo.url;
-                });
-                let url;
-                if (found) {
-                    url = found;
-                } else {
-                    const newKey = getRandomString();
-                    setDbKeyValue('links', newKey, {
-                        url: bestVideo.url,
-                    });
-                    url = newKey;
+            const args = [
+                '--dump-single-json',
+                '--no-warnings',
+                '--no-call-home',
+                '--no-check-certificate',
+                '--prefer-free-formats',
+                '--youtube-skip-dash-manifest',
+                `--referer=${messageContent}`
+            ].join(' ');
+            exec(`yt-dlp ${messageContent} ${args}`, (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
                 }
-
-                sendMsg(api, msg.threadID, `${config.domain}link/${url}`);
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
             });
+            // youtubedl(messageContent, {
+            //     dumpSingleJson: true,
+            //     noCheckCertificates: true,
+            //     noWarnings: true,
+            //     preferFreeFormats: true,
+            //     addHeader: [
+            //         'referer:youtube.com',
+            //         'user-agent:googlebot',
+            //     ],
+            //
+            // }).then(output => {
+            //     console.info(output);
+            //     const formats = output.formats.filter(f =>
+            //         f.audio_ext === 'none' && f.format_note && f.audio_channels,
+            //     ).map(f => ({
+            //         width: f.width,
+            //         height: f.height,
+            //         url: f.url,
+            //         format_note: f.format_note,
+            //         quality: f.quality,
+            //         fps: f.fps,
+            //     }));
+            //
+            //     const bestVideo = formats.reduce((best, current) => {
+            //         return current.quality > best.quality ? current : best;
+            //     }, formats[0]);
+            //
+            //     const found = Object.keys(linksDb).find(key => {
+            //         return linksDb[key].url === bestVideo.url;
+            //     });
+            //     let url;
+            //     if (found) {
+            //         url = found;
+            //     } else {
+            //         const newKey = getRandomString();
+            //         setDbKeyValue('links', newKey, {
+            //             url: bestVideo.url,
+            //         });
+            //         url = newKey;
+            //     }
+            //
+            //     sendMsg(api, msg.threadID, `${config.domain}link/${url}`);
+            // });
 
             return new Promise((resolve, reject) => {
                 resolve(null);
             });
         } catch (error) {
             sendMsg(api, msg.threadID, `[${cmd}] Błąd: coś się zepsuło...`);
-
+            console.error(error);
             return new Promise((resolve, reject) => {
                 resolve(null);
             });
