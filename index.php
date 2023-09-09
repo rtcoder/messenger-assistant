5ca67d4563e910f177b5553b6a0c749a0b629610
Dawid Jeż <dawidjez@gmail.com>
2023-09-09 19:39:04 +0200

    redirect to link

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+echo __DIR__;
+$path = $_SERVER['REQUEST_URI'];
+
+$parts = array_values(
+    array_filter(
+        explode('/', $path)
+    )
+);
+
+if ($parts[0] === 'link') {
+    try {
+        $linksDb = file_get_contents(__DIR__ . '/db/links.json');
+        $dbParsed = json_decode($linksDb, true);
+        if (isset($dbParsed[$parts[1]])) {
+            $url = $dbParsed[$parts[1]]['url'];
+            header('Location: ' . $url);
+            exit();
+        }
+    } catch (Throwable $exception) {
+        require_once 'views/404.html';
+    }
+}
