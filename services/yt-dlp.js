f5d97bac02bf2c04d028cf3605b5eff7b8e00169
Dawid Jeż <dawidjez@gmail.com>
2023-09-09 21:11:19 +0200

    fix download

--- services/yt-dlp.js
+++ services/yt-dlp.js
@@ -1,5 +1,4 @@
 const sendMsg = require('../utils/send-msg');
-const youtubedl = require('youtube-dl-exec');
 const {getDb, setDbKeyValue} = require('../utils/db');
 const {getRandomString} = require('../utils/string-helpers');
 const getConfig = require('../utils/config');
@@ -17,62 +16,51 @@ module.exports = {
                 '--no-check-certificate',
                 '--prefer-free-formats',
                 '--youtube-skip-dash-manifest',
-                `--referer=${messageContent}`
+                `--referer=${messageContent}`,
             ].join(' ');
-            exec(`yt-dlp ${messageContent} ${args}`, (error, stdout, stderr) => {
-                if (error) {
-                    console.log(`error: ${error.message}`);
-                    return;
-                }
-                if (stderr) {
-                    console.log(`stderr: ${stderr}`);
-                    return;
-                }
-                console.log(`stdout: ${stdout}`);
-            });
-            // youtubedl(messageContent, {
-            //     dumpSingleJson: true,
-            //     noCheckCertificates: true,
-            //     noWarnings: true,
-            //     preferFreeFormats: true,
-            //     addHeader: [
-            //         'referer:youtube.com',
-            //         'user-agent:googlebot',
-            //     ],
-            //
-            // }).then(output => {
-            //     console.info(output);
-            //     const formats = output.formats.filter(f =>
-            //         f.audio_ext === 'none' && f.format_note && f.audio_channels,
-            //     ).map(f => ({
-            //         width: f.width,
-            //         height: f.height,
-            //         url: f.url,
-            //         format_note: f.format_note,
-            //         quality: f.quality,
-            //         fps: f.fps,
-            //     }));
-            //
-            //     const bestVideo = formats.reduce((best, current) => {
-            //         return current.quality > best.quality ? current : best;
-            //     }, formats[0]);
-            //
-            //     const found = Object.keys(linksDb).find(key => {
-            //         return linksDb[key].url === bestVideo.url;
-            //     });
-            //     let url;
-            //     if (found) {
-            //         url = found;
-            //     } else {
-            //         const newKey = getRandomString();
-            //         setDbKeyValue('links', newKey, {
-            //             url: bestVideo.url,
-            //         });
-            //         url = newKey;
-            //     }
-            //
-            //     sendMsg(api, msg.threadID, `${config.domain}link/${url}`);
-            // });
+
+            exec(
+                `yt-dlp ${messageContent} ${args}`,
+                (error, stdout, stderr) => {
+                    if (error) {
+                        console.log(`error: ${error.message}`);
+                        return;
+                    }
+                    if (stderr) {
+                        console.log(`stderr: ${stderr}`);
+                        return;
+                    }
+                    const output = JSON.parse(stdout);
+                    const formats = output.formats.filter(f =>
+                        f.audio_ext === 'none' && f.format_note && f.audio_channels,
+                    ).map(f => ({
+                        width: f.width,
+                        height: f.height,
+                        url: f.url,
+                        format_note: f.format_note,
+                        quality: f.quality,
+                        fps: f.fps,
+                    }));
+                    const bestVideo = formats.reduce((best, current) => {
+                        return current.quality > best.quality ? current : best;
+                    }, formats[0]);
+
+                    const found = Object.keys(linksDb).find(key => {
+                        return linksDb[key].url === bestVideo.url;
+                    });
+                    let url;
+                    if (found) {
+                        url = found;
+                    } else {
+                        const newKey = getRandomString();
+                        setDbKeyValue('links', newKey, {
+                            url: bestVideo.url,
+                        });
+                        url = newKey;
+                    }
+
+                    sendMsg(api, msg.threadID, `${config.domain}link/${url}`);
+                });
 
             return new Promise((resolve, reject) => {
                 resolve(null);
