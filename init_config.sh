4565232831e55ab01f6267043b4686c900aef34a
Dawid Jeż <dawidjez@gmail.com>
2023-09-09 19:21:19 +0200

    remove config files

---
+++ init_config.sh
@@ -0,0 +1,15 @@
+if [ ! -f config/config.json ]; then
+  cat ./templates/config/config.json.tpl >>config/config.json
+fi
+
+if [ ! -f config/app-state ]; then
+  touch config/app-state.json
+fi
+
+if [ ! -f db/bot-info.json ]; then
+  cat ./templates/db/bot-info.json.tpl >>db/bot-info.json
+fi
+
+if [ ! -f db/list.json ]; then
+  echo "[]" >db/list.json
+fi
---
+++ templates/config/config.json.tpl
@@ -0,0 +1,7 @@
+{
+    "domain": "",
+    "allow_private_messages": true,
+    "bot_response_prefix": "",
+    "bot_user_id": "",
+    "threads_ids": []
+}
